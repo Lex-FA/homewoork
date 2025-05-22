3ad0924d4a78934ec21b573c32a5019f0628df66
Lex-FA <aleksejfilippov36687@gmail.com>
2025-05-22 19:44:49 +0400

    Delete HeadphoneWebStore directory

--- HeadphoneWebStore/src/Components/cartList.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import CartList from "./basket";
-
-const Cart: FC = () => {
-   return <CartList />;
-};
-
-export default Cart;
--- HeadphoneWebStore/src/Components/catalogPage.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-import Headphones from "./body";
-
-const CatalogPage: FC = () => {
-   return <Headphones />;
-};
-export default CatalogPage;
